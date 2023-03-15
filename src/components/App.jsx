27716908e79d7bb7bdc6abc4ed451d317e5439e3
ThisNarciss <tyomaxxx000@gmail.com>
2023-03-15 16:04:56 +0200

    add redux toolkit logic in pnonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+import { Section } from 'components/Section/Section';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { ChildrenBox, Container, Notification } from './App.styled';
+import { useSelector } from 'react-redux';
+
+export function App() {
+  const { contacts } = useSelector(state => state.contacts);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        {contacts.length ? (
+          <ChildrenBox>
+            <Filter />
+            <ContactList />
+          </ChildrenBox>
+        ) : (
+          <Notification>There are no contacts in the phone book</Notification>
+        )}
+      </Section>
+    </Container>
   );
-};
+}
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,47 @@
+import { nanoid } from 'nanoid';
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const contactsInitialState = {
+  contacts: [],
+};
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        return { ...state, contacts: [...state.contacts, action.payload] };
+      },
+      prepare(obj) {
+        return {
+          payload: {
+            id: nanoid(),
+            ...obj,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        ),
+      };
+    },
+  },
+});
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+export const contactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
+export const { addContact, deleteContact } = contactsSlice.actions;
